8899a3c79975432716f4570b4d53e0ac84dc74f9
TechExplorer15 <techexplorer015@gmail.com>
2026-06-11 14:25:05 +0530

    Fix Lenis RAF memory leak and optimize scroll config

--- lms-frontend/src/components/common/SmoothScroll.jsx
+++ lms-frontend/src/components/common/SmoothScroll.jsx
@@ -12,25 +12,28 @@ export function SmoothScroll({ children }) {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
+      smoothWheel: true,
+      wheelMultiplier: 1.1,
       smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
 
+    let rafId;
+
     function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      rafId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    rafId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(rafId);
       lenis.destroy();
     };
   }, []);
